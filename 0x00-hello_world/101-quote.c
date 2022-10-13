697b7cc98d29cc0cd6798e104251eab2a4cc49f5
Kaleab Mesfin <kaleabmesfinq@gmail.com>
2022-10-13 12:45:15 -0700

    Shell script file created

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main	- Entry point
+ *
+ * Description: 'and that piece of art is usefull" - Kaleab Mesfin, 2021-13-10'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
