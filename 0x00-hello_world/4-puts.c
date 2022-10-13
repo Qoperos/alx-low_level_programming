aa9b5948bc8d31c46cfa61fd34f7e8a69e9829f3
Kaleab Mesfin <kaleabmesfinq@gmail.com>
2022-10-13 09:11:28 -0700

    C program created with puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-/* prints some world using puts function */
+/** 
+ * main - prints some world using puts function
+ *
+ * Description : using the main function this program outputs
+ * or prints "Programming is like building a multilingual puzzle
+ * Return : 0
+ */
 
 int main (void)
 {
