41ea0b018bdc9255f73023b1d25384d549ff43e5
Kaleab Mesfin <kaleabmesfinq@gmail.com>
2022-10-13 09:03:47 -0700

    4-put file modified according to betty's style coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+
 /* prints some world using puts function */
 
-int main (void) {
+int main (void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
