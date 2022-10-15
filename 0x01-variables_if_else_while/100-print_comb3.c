e9794e12be83ec0dd600ff25f29022676b8052be
Kaleab Mesfin <kaleabmesfinq@gmail.com>
2022-10-14 21:10:59 -0700

    100-print_comb3 file created

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,59 +1,30 @@
 #include <stdio.h>
 
-
-
 /**
+ * main	- Print combinations of two digit numbers
  *
- * * main - Print numbers from 00 to 99
- *
- * *
- *
- * * Return: Always 0 (Success)
- *
- * */
-
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
+ 	int tens;
+	int ones;
 
-		int tens;
+       	for (tens = 0; tens <= 9; tens++)
+	{
+		for (ones = tens + 1; ones <= 9; ones++)
+		{
+			putchar(tens + '0');
+			putchar(ones + '0');
 
-			int ones;
+			if (tens < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
 
-
-
-					for (tens = 0; tens <= 9; tens++)
-
-						   		 {
-
-									 		 for (ones = 0; ones <= 9; ones++)
-
-												 	{
-
-															  putchar(tens + '0');
-
-															  	  putchar(ones + '0');
-
-
-
-																  	  if (!(tens == 9 && ones == 9))
-
-																		  	    {
-
-																				    	      putchar(',');
-
-																					      	      putchar(' ');
-
-																						      	    }
-
-																	  	}
-
-											     }
-
-					  putchar('\n');
-
-
-
-					    return (0);
-
+	return (0);
 }
