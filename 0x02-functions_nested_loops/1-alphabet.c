f96408e4926916839b569f6f0c1f7b19acf2cb89
Kaleab Mesfin <kaleabmesfinq@gmail.com>
2022-10-18 10:28:20 -0700

    1-alphabet file created

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main	- Print alphabet from a to z.
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+char c;
+for(c = 97; c < 123; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
