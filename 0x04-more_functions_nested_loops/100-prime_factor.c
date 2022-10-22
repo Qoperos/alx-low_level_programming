3e09a3aac68f21e4fd1f700399fff8c73597e1ba
Kaleab Mesfin <kaleabmesfinq@gmail.com>
2022-10-21 19:25:12 -0700

    100-prime_factor file modified

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,27 +1,26 @@
 #include <stdio.h>
 
 /**
+ * main - entry block
  *
- * * main - entry block
+ * @void: no argument
  *
- * * @void: no argument
+ * Return: 0
  *
- * * Return: 0
- *
- * */
+ */
 int main(void)
 {
 
 	long i, number = 612852475143;
 
 	for (i  = 2; i <= number; i++)
+	{
+		if (number % i == 0)
 		{
-			if (number % i == 0)
-			{
-				number = number / i;
-				i--;
-			}
+			number = number / i;
+			i--;
 		}
-		printf("%lu\n", i);
-		return (0);
+	}
+	printf("%lu\n", i);
+	return (0);
 }
